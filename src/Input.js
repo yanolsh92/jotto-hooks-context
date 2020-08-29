521fde686c2a74885e9bdb668a6f5e051972816e
yanolsh92 <yanolsh@gmail.com>
2020-08-29 10:30:39 +0300

    PropTypes in Input Component

--- src/Input.js
+++ src/Input.js
@@ -1,7 +1,10 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 const Input = (props) => {
   return <div data-test='component-input'></div>;
 };
 
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired,
+};
 export default Input;
--- src/Input.test.js
+++ src/Input.test.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { findByTestAttr } from '../test/testUtils';
+import { findByTestAttr, checkProps } from '../test/testUtils';
 import Input from './Input';
 
 /**
  * Setup function for input component
  * @returns {ShallowWrapper}
  */
-const setup = () => {
-  return shallow(<Input />);
+const setup = (secretWord = 'party') => {
+  return shallow(<Input secretWord={secretWord} />);
 };
 test('Input renders without error', () => {
   const wrapper = setup();
   const component = findByTestAttr(wrapper, 'component-input');
   expect(component.length).toBe(1);
+});
+
+test('does not thro waring with expected props', () => {
+  checkProps(Input, { secretWord: 'party' });
 });
