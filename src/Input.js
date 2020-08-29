7d08ff16848a252c2ed61cec42394ccff038f6ae
yanolsh92 <yanolsh@gmail.com>
2020-08-29 10:48:03 +0300

    Test input setState

--- src/Input.js
+++ src/Input.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 const Input = (props) => {
-  return <div data-test='component-input'></div>;
+  const [currentGuess, setCurrentGuess] = React.useState('');
+  return (
+    <div data-test='component-input'>
+      <form className='form-inline'>
+        <input
+          data-test='input-box'
+          className='mb-2 mx-sm-3'
+          type='text'
+          placeholder='enter guess'
+          value={currentGuess}
+          onChange={(event) => setCurrentGuess(event.target.value)}
+        />
+
+        <button
+          data-test='submit-button'
+          className='btn btn-primary mb-2'
+          onClick={(evt) => {
+            evt.preventDefault();
+          }}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 Input.propTypes = {
