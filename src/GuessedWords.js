0e8ea5ee604fa4d429b27e2cc8cdb733a15157aa
yanolsh92 <yanolsh@gmail.com>
2020-08-29 15:46:03 +0300

    guessedWords language context

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -1,40 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import languageContext from './contexts/languageContext';
+import stringsModule from './helpers/strings';
 const GuessedWords = (props) => {
-  let contents
+  const language = React.useContext(languageContext);
+  let contents;
   if (props.guessedWords.length === 0) {
     contents = (
-      <span data-test="guess-instructions">
-        Try to guess the secret word!
+      <span data-test='guess-instructions'>
+        {stringsModule.getStringByLanguage(language, 'guessPrompt')}{' '}
       </span>
     );
   } else {
     const guessedWordsRows = props.guessedWords.map((word, index) => (
-      <tr data-test="guessed-word" key={ index }>
-        <td>{ word.guessedWord }</td>
-        <td>{ word.letterMatchCount }</td>
+      <tr data-test='guessed-word' key={index}>
+        <td>{word.guessedWord}</td>
+        <td>{word.letterMatchCount}</td>
       </tr>
     ));
     contents = (
-      <div data-test="guessed-words">
-        <h3>Guessed Words</h3>
-        <table className="table table-sm">
-          <thead className="thead-light">
-            <tr><th>Guess</th><th>Matching Letters</th></tr>
+      <div data-test='guessed-words'>
+        <h3>{stringsModule.getStringByLanguage(language, 'guessedWords')}</h3>
+        <table className='table table-sm'>
+          <thead className='thead-light'>
+            <tr>
+              <th>
+                {stringsModule.getStringByLanguage(
+                  language,
+                  'guessColumnHeader'
+                )}
+              </th>
+              <th>
+                {stringsModule.getStringByLanguage(
+                  language,
+                  'matchingLettersColumnHeader'
+                )}
+              </th>
+            </tr>
           </thead>
-          <tbody>
-            { guessedWordsRows }
-          </tbody>
+          <tbody>{guessedWordsRows}</tbody>
         </table>
       </div>
     );
   }
-  return (
-    <div data-test="component-guessed-words">
-      { contents }
-    </div>
-  );
+  return <div data-test='component-guessed-words'>{contents}</div>;
 };
 
 GuessedWords.propTypes = {
--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -8,14 +8,14 @@ const defaultProps = {
 };
 
 /**
-* Factory function to create a ShallowWrapper for the GuessedWords component.
-* @function setup
-* @param {object} props - Component props specific to this setup.
-* @returns {ShallowWrapper}
-*/
-const setup = (props={}) => {
+ * Factory function to create a ShallowWrapper for the GuessedWords component.
+ * @function setup
+ * @param {object} props - Component props specific to this setup.
+ * @returns {ShallowWrapper}
+ */
+const setup = (props = {}) => {
   const setupProps = { ...defaultProps, ...props };
-  return shallow(<GuessedWords {...setupProps } />)
+  return shallow(<GuessedWords {...setupProps} />);
 };
 
 test('does not throw warning with expected props', () => {
@@ -23,7 +23,7 @@ test('does not throw warning with expected props', () => {
 });
 
 describe('if there are no words guessed', () => {
-  let wrapper
+  let wrapper;
   beforeEach(() => {
     wrapper = setup({ guessedWords: [] });
   });
@@ -46,7 +46,7 @@ describe('if there are words guessed', () => {
   beforeEach(() => {
     wrapper = setup({ guessedWords });
   });
-  test ('renders without error', () => {
+  test('renders without error', () => {
     const component = findByTestAttr(wrapper, 'component-guessed-words');
     expect(component.length).toBe(1);
   });
@@ -57,5 +57,20 @@ describe('if there are words guessed', () => {
   test('correct number of guessed words', () => {
     const guessedWordNodes = findByTestAttr(wrapper, 'guessed-word');
     expect(guessedWordNodes.length).toBe(guessedWords.length);
+  });
+});
+describe('languagePicker', () => {
+  test('correctly renders guess instructions string in English by default', () => {
+    const wrapper = setup({ guessedWords: [] });
+    const guessInstructions = findByTestAttr(wrapper, 'guess-instructions');
+
+    expect(guessInstructions.text()).toBe('Try to guess the secret word! ');
+  });
+  test('correctly renders guess instructions string in emoji', () => {
+    const mockUseContext = jest.fn().mockReturnValue('emoji');
+    React.useContext = mockUseContext;
+    const wrapper = setup({ guessedWords: [] });
+    const guessInstructions = findByTestAttr(wrapper, 'guess-instructions');
+    expect(guessInstructions.text()).toBe(`🤔🤫🔤 `);
   });
 });
