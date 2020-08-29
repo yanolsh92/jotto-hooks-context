5d77ff94c6179f244077cec13e24f1e3090790f7
yanolsh92 <yanolsh@gmail.com>
2020-08-29 16:57:38 +0300

    add test to App

--- src/App.js
+++ src/App.js
@@ -59,6 +59,7 @@ function App() {
   return (
     <div className='container' data-test='component-app'>
       <h1>Jotto</h1>
+      <p>The secret word is {state.secretWord}</p>
       <languageContext.Provider value={state.language}>
         <LanguagePicker setLanguage={setLanguage} />
         <guessedWordsContext.GuessedWordsProvider>
