acafb9c31a2b843337ff156ba91b34d3f3c00671
yanolsh92 <yanolsh@gmail.com>
2020-08-29 10:26:26 +0300

    Basic Input Component

---
+++ src/Input.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { findByTestAttr } from '../test/testUtils';
+import Input from './Input';
+
+/**
+ * Setup function for input component
+ * @returns {ShallowWrapper}
+ */
+const setup = () => {
+  return shallow(<Input />);
+};
+test('Input renders without error', () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, 'component-input');
+  expect(component.length).toBe(1);
+});
