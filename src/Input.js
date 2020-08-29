89ffce45d9a77350ff560e8e299559a72207628a
yanolsh92 <yanolsh@gmail.com>
2020-08-29 16:43:03 +0300

    geussess.test

--- src/Input.js
+++ src/Input.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
+import guessedWordsContext from './contexts/guessedWordsContext';
 import successContext from './contexts/successContext';
 import languageContext from './contexts/languageContext';
 import stringsModule from './helpers/strings';
-const Input = (props) => {
+import { getLetterMatchCount } from './helpers';
+
+function Input({ secretWord }) {
   const language = React.useContext(languageContext);
   const [success, setSuccess] = successContext.useSuccess();
+  const [guessedWords, setGuessedWords] = guessedWordsContext.useGuessedWords();
   const [currentGuess, setCurrentGuess] = React.useState('');
 
   if (success) {
     return null;
   }
+
   return (
     <div data-test='component-input'>
       <form className='form-inline'>
@@ -25,25 +31,39 @@ const Input = (props) => {
           value={currentGuess}
           onChange={(event) => setCurrentGuess(event.target.value)}
         />
-
         <button
           data-test='submit-button'
-          className='btn btn-primary mb-2'
           onClick={(evt) => {
             evt.preventDefault();
-            //TODO: update guessedWords
-            //TODO: check against secretWord and update succes if needed
+            // update guessedWords
+            const letterMatchCount = getLetterMatchCount(
+              currentGuess,
+              secretWord
+            );
+            const newGuessedWords = [
+              ...guessedWords,
+              { guessedWord: currentGuess, letterMatchCount },
+            ];
+            setGuessedWords(newGuessedWords);
+
+            // check against secretWord and update success if needed
+            if (currentGuess === secretWord) {
+              setSuccess(true);
+            }
+            // clear input box
             setCurrentGuess('');
           }}
+          className='btn btn-primary mb-2'
         >
           {stringsModule.getStringByLanguage(language, 'submit')}
         </button>
       </form>
     </div>
   );
-};
+}
 
 Input.propTypes = {
   secretWord: PropTypes.string.isRequired,
 };
+
 export default Input;
